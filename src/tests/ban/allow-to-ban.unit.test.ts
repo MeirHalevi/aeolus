9363f7f9307d40b7b9cae24799b7c5c4d095934c
matangilad <matan.gilad@fiverr.com>
2021-10-26 14:56:31 +0300

    [TEST] fail-to-ban & allow-to-ban when there is cache

--- src/tests/ban/allow-to-ban.unit.test.ts
+++ src/tests/ban/allow-to-ban.unit.test.ts
@@ -3,9 +3,9 @@ import { AllowToBan } from '../../index';
 import * as sinon from 'sinon';
 
 describe('AllowToBan tests', () => {
-    describe('Without a cache defined in it', () => {
-        const allowToBan = new AllowToBan();
+    const allowToBan = new AllowToBan();
 
+    describe('Without a cache defined in it', () => {
         describe('filter method', () => {
             it('should reject filter method', async () => {
                 const callbackRule: () => boolean = sinon.fake.returns(true);
@@ -16,6 +16,25 @@ describe('AllowToBan tests', () => {
         describe('reset method', () => {
             it('should reject reset method', async () => {
                 await expect(allowToBan.reset('test', 120)).to.be.rejectedWith(Error);
+            });
+        });
+    });
+
+    describe('With a cache defined in it', () => {
+        describe('filter method', () => {
+            sinon.stub(allowToBan as any, 'isBanned').returns(Promise.resolve(false));
+
+            it('should return false when not banned & callback is true', async () => {
+                const callbackRule = sinon.fake.returns(true);
+                sinon.stub(allowToBan as any, 'fail').returns(Promise.resolve(false));
+
+                await expect(allowToBan.filter('test', 120, 120, 5, callbackRule)).to.eventually.be.false;
+            });
+
+            it('should return false when not banned & callback is false', async () => {
+                const callbackRule = sinon.fake.returns(false);
+
+                await expect(allowToBan.filter('test', 120, 120, 5, callbackRule)).to.eventually.be.false;
             });
         });
     });
--- src/tests/ban/fail-to-ban.unit.test.ts
+++ src/tests/ban/fail-to-ban.unit.test.ts
@@ -3,12 +3,13 @@ import { FailToBan } from '../../index';
 import * as sinon from 'sinon';
 
 describe('FailToBan tests', () => {
-    describe('Without a cache defined in it', () => {
-        const failToBan = new FailToBan();
+    const failToBan = new FailToBan();
 
+    describe('Without a cache defined in it', () => {
         describe('filter method', () => {
             it('should reject filter method', async () => {
                 const callbackRule = sinon.fake.returns(true);
+
                 await expect(failToBan.filter('test', 120, 120, 5, callbackRule)).to.be.rejectedWith(Error);
             });
         });
@@ -16,6 +17,25 @@ describe('FailToBan tests', () => {
         describe('reset method', () => {
             it('should reject reset method', async () => {
                 await expect(failToBan.reset('test', 120)).to.be.rejectedWith(Error);
+            });
+        });
+    });
+
+    describe('With a cache defined in it', () => {
+        describe('filter method', () => {
+            sinon.stub(failToBan as any, 'isBanned').returns(Promise.resolve(false));
+
+            it('should return true when not banned & callback is true', async () => {
+                const callbackRule = sinon.fake.returns(true);
+                sinon.stub(failToBan as any, 'fail').returns(Promise.resolve(true));
+
+                await expect(failToBan.filter('test', 120, 120, 5, callbackRule)).to.eventually.be.true;
+            });
+
+            it('should return false when not banned & callback is false', async () => {
+                const callbackRule = sinon.fake.returns(false);
+
+                await expect(failToBan.filter('test', 120, 120, 5, callbackRule)).to.eventually.be.false;
             });
         });
     });
